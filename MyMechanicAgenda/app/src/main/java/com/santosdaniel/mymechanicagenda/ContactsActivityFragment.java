69a69e64496b05048921614322495500aa67f3df
sants <31745313+santosdaniel@users.noreply.github.com>
2017-09-08 22:47:31 +0200

    Initial commit

---
+++ MyMechanicAgenda/app/src/main/java/com/santosdaniel/mymechanicagenda/ContactsActivityFragment.java
@@ -0,0 +1,22 @@
+package com.santosdaniel.mymechanicagenda;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ContactsActivityFragment extends Fragment {
+
+    public ContactsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+}
