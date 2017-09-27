0eec79590866eae5e0e6aa3e4f57ae62ea14f11e
sants <31745313+santosdaniel@users.noreply.github.com>
2017-09-27 18:50:18 +0200

    Refactor contact details fragment.

--- MyMechanicAgenda/app/src/main/java/com/santosdaniel/mymechanicagenda/view/contact_details/ContactDetailsActivity.java
+++ MyMechanicAgenda/app/src/main/java/com/santosdaniel/mymechanicagenda/view/contact_details/ContactDetailsActivity.java
@@ -30,108 +30,29 @@ import java.util.List;
  */
 public class ContactDetailsActivity extends GenericActivity<ContactListModel> {
 
-    public static final String TAG = "DaoExample";
-    private EditText editText;
-    private View addNoteButton;
 
-    private ContactRepository contactRepository;
-    private NotesAdapter notesAdapter;
+    /**
+     * Initializes the model that is going to use in the activity
+     *
+     * @param savedInstanceState object containing the activity's previously saved state
+     */
+    private void setModel(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            ContactListModel model = new ContactListModel();
+            super.setModel(model);
+        }
+    }
 
+    /**
+     * Called on create of the activity
+     *
+     * @param savedInstanceState    object containing the activity's previously saved state
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
-        setToolbarWithTitle(R.string.phone_book, true);
-
-        setUpViews();
-
-        IDaoSessionProvider daoSessionProvider = ((IDaoSessionProvider) getApplication());
-        this.contactRepository = new ContactRepository(daoSessionProvider);
-
-
-        updateNotes();
+        setToolbarWithTitle(R.string.phone_book, false);
+        setModel(savedInstanceState);
     }
-
-    private void updateNotes() {
-        List<Note> notes = this.contactRepository.getAllContacts();
-        notesAdapter.setNotes(notes);
-    }
-
-    protected void setUpViews() {
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewNotes);
-        //noinspection ConstantConditions
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        notesAdapter = new NotesAdapter(noteClickListener);
-        recyclerView.setAdapter(notesAdapter);
-
-        addNoteButton = findViewById(R.id.buttonAdd);
-        //noinspection ConstantConditions
-        addNoteButton.setEnabled(false);
-
-        editText = (EditText) findViewById(R.id.editTextNote);
-        editText.setOnEditorActionListener(new OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    addNote();
-                    return true;
-                }
-                return false;
-            }
-        });
-        editText.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                boolean enable = s.length() != 0;
-                addNoteButton.setEnabled(enable);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-    }
-
-    public void onAddButtonClick(View view) {
-        addNote();
-    }
-
-    private void addNote() {
-        String noteText = editText.getText().toString();
-        editText.setText("");
-
-        final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
-        String comment = "Added on " + df.format(new Date());
-
-        Note note = new Note();
-        note.setText(noteText);
-        note.setComment(comment);
-        note.setDate(new Date());
-        note.setType(NoteType.TEXT);
-        this.contactRepository.create(note);
-        Log.d(TAG, "Inserted new note, ID: " + note.getId());
-
-        updateNotes();
-    }
-
-    NotesAdapter.NoteClickListener noteClickListener = new NotesAdapter.NoteClickListener() {
-        @Override
-        public void onNoteClick(int position) {
-            Note note = notesAdapter.getNote(position);
-            Long noteId = note.getId();
-
-            contactRepository.deleteById(noteId);
-            Log.d(TAG, "Deleted note, ID: " + noteId);
-
-            updateNotes();
-        }
-    };
 }
---
+++ MyMechanicAgenda/app/src/main/java/com/santosdaniel/mymechanicagenda/view/contact_details/ContactDetailsFragment.java
@@ -0,0 +1,153 @@
+package com.santosdaniel.mymechanicagenda.view.contact_details;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import com.santosdaniel.mymechanicagenda.R;
+import com.santosdaniel.mymechanicagenda.model.database.Note;
+import com.santosdaniel.mymechanicagenda.model.database.NoteType;
+import com.santosdaniel.mymechanicagenda.presenter.contact_details.ContactRepository;
+import com.santosdaniel.mymechanicagenda.presenter.contact_details.NotesAdapter;
+import com.santosdaniel.mymechanicagenda.presenter.generic_database.IDaoSessionProvider;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import android.support.v4.app.Fragment;
+
+/**
+ * Activity to present the contact with a list of vehicles
+ */
+public class ContactDetailsFragment extends Fragment {
+
+    public static final String TAG = "DaoExample";
+    private EditText editText;
+    private View addNoteButton;
+
+    private ContactRepository contactRepository;
+    private NotesAdapter notesAdapter;
+
+    /**
+     * Find the views that is going to use in the fragment
+     *
+     * @param fragmentView Reference to the view of the fragment
+     */
+    private void bindViews(View fragmentView) {
+        RecyclerView recyclerView = (RecyclerView) fragmentView.findViewById(R.id.recyclerViewNotes);
+        //noinspection ConstantConditions
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        notesAdapter = new NotesAdapter(noteClickListener);
+        recyclerView.setAdapter(notesAdapter);
+
+        addNoteButton = fragmentView.findViewById(R.id.buttonAdd);
+        //noinspection ConstantConditions
+        addNoteButton.setEnabled(false);
+
+        editText = (EditText) fragmentView.findViewById(R.id.editTextNote);
+        editText.setOnEditorActionListener(new OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    addNote();
+                    return true;
+                }
+                return false;
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean enable = s.length() != 0;
+                addNoteButton.setEnabled(enable);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    /**
+     * @param inflater  The inflater of the view
+     * @param container The container where is to inflate the fragment
+     * @param savedInstanceState    The state previously saved
+     *
+     * @return  The view that is going support the fragment
+     */
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.fragment_contact_details, container, false);
+        bindViews(fragmentView);
+
+        IDaoSessionProvider daoSessionProvider = ((IDaoSessionProvider) getActivity().getApplication());
+        this.contactRepository = new ContactRepository(daoSessionProvider);
+
+        updateNotes();
+        return fragmentView;
+    }
+
+    private void updateNotes() {
+        List<Note> notes = this.contactRepository.getAllContacts();
+        notesAdapter.setNotes(notes);
+    }
+
+
+
+    public void onAddButtonClick(View view) {
+        addNote();
+    }
+
+    private void addNote() {
+        String noteText = editText.getText().toString();
+        editText.setText("");
+
+        final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        String comment = "Added on " + df.format(new Date());
+
+        Note note = new Note();
+        note.setText(noteText);
+        note.setComment(comment);
+        note.setDate(new Date());
+        note.setType(NoteType.TEXT);
+        this.contactRepository.create(note);
+        Log.d(TAG, "Inserted new note, ID: " + note.getId());
+
+        updateNotes();
+    }
+
+    NotesAdapter.NoteClickListener noteClickListener = new NotesAdapter.NoteClickListener() {
+        @Override
+        public void onNoteClick(int position) {
+            Note note = notesAdapter.getNote(position);
+            Long noteId = note.getId();
+
+            contactRepository.deleteById(noteId);
+            Log.d(TAG, "Deleted note, ID: " + noteId);
+
+            updateNotes();
+        }
+    };
+}
